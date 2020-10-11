ef4627f58838b13b26b09bed6a6298d7d01966e2
Rajat Chandra <rajatc19@gmail.com>
2020-10-11 14:02:11 +0530

    Initial commit

---
+++ Server/start.java
@@ -0,0 +1,12 @@
+package Server;
+
+
+
+public class start{
+	public static void main(String[] args){
+		SetPassword frame1= new SetPassword();
+	    	frame1.setSize(300,80); 				
+	    	frame1.setLocation(500,300);
+		frame1.setVisible(true);	 
+	}
+}
